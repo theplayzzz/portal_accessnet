e7de13f10d6b92dfbee6d04ee6280c13fb459c30
theplayzzz <play-felix@hotmail.com>
2026-05-07 17:39:28 -0300

    This version of Antigravity is no longer supported. Please upgrade to receive the latest features.

--- config/stores.ts
+++ config/stores.ts
@@ -2,4 +2,10 @@ export const STORES = [
   { name: "São Bento", image: "/api/blob-img/accessnet/stores/sao-bento.webp", address: "São Bento - MA" },
   { name: "Santa Helena", image: "/api/blob-img/accessnet/stores/santa-helena.webp", address: "Santa Helena - MA" },
   { name: "Presidente Sarney", image: "/api/blob-img/accessnet/stores/presidente-sarney.webp", address: "Pres. Sarney - MA" },
+  { name: "Cururupu", image: "/api/blob-img/accessnet/stores/cururupu.webp", address: "Cururupu - MA" },
+  { name: "Governador Nunes Freire", image: "/api/blob-img/accessnet/stores/governador-nunes-freire.webp", address: "Gov. Nunes Freire - MA" },
+  { name: "Maracaçumé", image: "/api/blob-img/accessnet/stores/maracacume.webp", address: "Maracaçumé - MA" },
+  { name: "Palmerândia", image: "/api/blob-img/accessnet/stores/palmerandia.webp", address: "Palmeirândia - MA" },
+  { name: "Pinheiro", image: "/api/blob-img/accessnet/stores/pinheiro.webp", address: "Pinheiro - MA" },
+  { name: "Três Furos", image: "/api/blob-img/accessnet/stores/tres-furos.webp", address: "Três Furos - MA" },
 ];
